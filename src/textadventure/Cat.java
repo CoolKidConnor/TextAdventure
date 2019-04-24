8e67d1203bdb5dc9d135db29a324b67a988cf3da
unknown <hunter.barclay18@gmail.com>
2019-04-23 17:43:21 -0700

    Ethan oofed my code

---
+++ src/textadventure/Cat.java
@@ -0,0 +1,12 @@
+package textadventure;
+public class Cat{
+    int numberOfPaws;
+    public Cat(){
+        numberOfPaws = 5;
+    }
+    
+
+    /*public void eat(){
+        hunger++;
+    }*/
+}
--- src/textadventure/GameLoop.java
+++ src/textadventure/GameLoop.java
@@ -1,39 +1,46 @@
-//package textadventure;
+package textadventure;
 import java.util.Scanner;
+import textadventure.Cat;
 
 public class GameLoop{
 static boolean gamerunning = true;
     static int xcord = 0;
     static int ycord = 0;
-    
+    static Scanner input = new Scanner(System.in);
+    static Cat percy = new Cat();
+
 public static void main(String[] args) {
-    // java -cp C:/users/chsrobotics/git/TextAdventure/bin GameLoop
-    //$ javac -d bin src/textadventure/GameLoop.java
-    Scanner input = new Scanner(System.in);
+    // cd src, textadventure,GameLoop
+    // javac GameLoop.java
+    //java GameLoop
     //System.out.println("What is your name?");
     //String name = input.nextLine();
     //System.out.println("Your name is " + name);
     gamerunning = true;
     while(gamerunning){
+        System.out.println(percy.numberOfPaws);
         System.out.println("What do you want to do?");
-        String action = input.nextLine();
-            if(action.equals("Right")){
-                xcord +=1;
-            }
-            else if (action.equals("Left")){
-                xcord -=1;
-            }
-            else if (action.equals("Up")){
-                ycord +=1;
-            }
-            else if (action.equals("Down")){
-                ycord -=1;
-            }
-            else if (action.equals("Position")){
-                System.out.println("Your position is (" + xcord + ", " + ycord + ")");
-            }
+        handleAction();
             //gamerunning = false;
+     }
     }
-          
-   }
+
+    public static void handleAction(){
+        String action = input.nextLine();
+        switch(action){
+            case "right":
+                xcord +=1;
+                System.out.println("You move right");
+                break;
+            case "left":
+                xcord -=1;
+                break;
+            case "up":
+                ycord += 1;
+                break;
+            case "down":
+            ycord -=1;
+            break;
+        }
+    }
 }
